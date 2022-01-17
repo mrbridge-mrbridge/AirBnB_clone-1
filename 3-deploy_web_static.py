98cbc0b9d78fa340e15a0e4eff45c8f75aee0e8c
root <nanamcroj@gmai.com>
2022-01-17 12:39:01 -0800

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,15 +3,15 @@
  do_pack: A Fabfile that archives all contents of web_static
 +Must be gzipped...thus tar -z
 +name of archive file must have creation time attached
-
  do_deploy: Deploys this zip to servers
+deploy: Does both
 """
 from fabric.api import local, run, env, put
 from datetime import datetime
-from os.path import isfile, split, splitext
+from os.path import isfile, split
 
 
-env.hosts = ['34.73.135.187', '3.237.43.34']
+env.hosts = ['34.138.138.111', '3.227.11.146']
 
 
 def do_pack():
@@ -26,6 +26,7 @@ def do_pack():
     except Exception:
         return None
 
+
 def do_deploy(archive_path):
     """deploys an archive to web-server-1 and web-server-2"""
     if not isfile(archive_path):
@@ -36,7 +37,7 @@ def do_deploy(archive_path):
         archive_file_ext = archive_path.split("/")[-1]
 
         # Get filename without extension
-        archive_file, ext = splitext("archive_file_ext")
+        archive_file = archive_file_ext.split(".")[0]
 
         # upload archive to recieved
         recieved = "/tmp/{}".format(archive_file_ext)
@@ -45,19 +46,20 @@ def do_deploy(archive_path):
         # extract archive into new directory that must be empty if exists
         new_dir_name = "/data/web_static/releases/{}/".format(archive_file)
         run("mkdir -p {}".format(new_dir_name))
-        run("tar -C {} -xzf {}".format(new_dir_name, recieved))
+        run("tar -xzf {} -C {}".format(recieved, new_dir_name))
 
         # delete archive from old folder
         run("rm {}".format(recieved))
 
-        run("mv {}/web_static/* {}".format(new_dir_name, new_dir_name))
-        run("rm -rf {}/web_static/".format(new_dir_name))
+        run("mv {}web_static/* {}".format(new_dir_name, new_dir_name))
+        run("rm -rf {}web_static/".format(new_dir_name))
 
         # delete old symlink
         run("rm -rf /data/web_static/current")
 
         # create new symlink to extracted files
-        run("ln -s new_dir_name /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+            format(archive_file))
 
         # when deployed successfully
         print('New version deployed!')
@@ -66,3 +68,14 @@ def do_deploy(archive_path):
         # in case of any failure
     except Exception:
         return False
+
+
+def deploy():
+    """do full deployment by calling do_pack and do_deploy"""
+
+    # call do_deploy
+    new_file = do_pack()
+    if not isfile(new_file):
+        return False
+
+    return do_deploy(archive_path=new_file)
