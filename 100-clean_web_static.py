8a237aebed294c7e498eaa294312976976127c0f
root <nanamcroj@gmai.com>
2022-01-17 18:51:57 -0800

    clear

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,9 @@ env.hosts = ['34.138.138.111', '3.227.11.146']
 
 
 def do_clean(number=0):
-    """deletes out-of-date archives"""
+    """deletes out-of-date archives
+        arg (number) = number of arhives to keep
+    """
     num = int(number)
     if num in (0, 1):
         num = 1
