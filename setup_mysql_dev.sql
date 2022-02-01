38f8a8f6fc3148cac153e9800f02508684399816
root <nanamcroj@gmail.com>
2022-02-01 18:02:20 +0000

    prepared mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
-# prepare a MySQL server
+-- prepare a MySQL server
 CREATE DATABASE hbnb_dev_db IF NOT EXISTS
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
